4f2664a2ae43ce4efddc5407ad9861a8eaf4a8a2
Witek Galecki <w.galecki@clearcode.cc>
2014-07-11 12:12:02 +0200

    Further gulpfile improvments

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,56 @@
 
   var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    concat = require('gulp-concat'),
     min = require('gulp-ngmin'),
     uglify = require('gulp-uglify'),
+    rimraf = require('gulp-rimraf'),
+    concat = require('gulp-concat'),
+    watch = require('gulp-watch'),
 
     paths = {
-      src: './src/*.js',
-      dist: './dist/ng-transiterate.min.js'
+      base: './src/',
+      src: './src/**/*.js',
+      dist: './dist/'
     };
 
   gulp.task('lint', function() {
-    return gulp.src(paths.src)
+    return gulp
+      .src(paths.src)
       .pipe(jshint())
       .pipe(jshint.reporter('jshint-stylish'));
   });
+
+  gulp.task('clean', function() {
+    return gulp
+      .src(paths.dist + '**/*.js', {
+        read: false
+      })
+      .pipe(rimraf());
+  });
+
+  gulp.task('copy', ['clean'], function() {
+    return gulp
+      .src(paths.src, {
+        base: paths.base
+      })
+      .pipe(gulp.dest(paths.dist));
+  });
+
+  gulp.task('minify', ['clean'], function() {
+    return gulp
+      .src(paths.src)
+      .pipe(min())
+      .pipe(uglify())
+      .pipe(concat('ng-transiterate.min.js'))
+      .pipe(gulp.dest(paths.dist));
+  });
+
+  gulp.task('watch', function() {
+    gulp.watch(paths.src, ['build']);
+  });
+
+  gulp.task('build', ['copy', 'minify']);
+
+  gulp.task('default', ['build', 'watch']);
 
 })(require);
